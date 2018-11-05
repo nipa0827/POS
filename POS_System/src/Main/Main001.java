5aecb719fb824e806ea5b284639fbe129693f3f0
suraviakhter <suravi.nipa@gmail.com>
2018-11-06 05:36:14 +0600

    POS complete project

---
+++ POS_System/src/Main/Main001.java
@@ -0,0 +1,13 @@
+package Main;
+
+import Frames.LogInPage;
+
+public class Main001 {
+
+	public static void main(String[] args) {
+		LogInPage myFirstPage = new LogInPage();
+		myFirstPage.run();
+
+	}
+
+}
